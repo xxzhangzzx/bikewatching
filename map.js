5eca1c7dacbe633fb6525c2fe903500729f7603e
xxzhangzzx <zhz194@ucsd.edu>
2026-05-15 22:59:58 -0700

    update

--- map.js
+++ map.js
@@ -1,6 +1,5 @@
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
-
-console.log('Mapbox GL JS Loaded:', mapboxgl);
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoieHh6aGFuZ3p6eCIsImEiOiJjbXA3d2JtcDYwN3JsMnNwdGQ3aGwyOWpoIn0.vPBp2gND5xjxIjqDDX_Z4Q';
 
@@ -13,13 +12,23 @@ const map = new mapboxgl.Map({
   maxZoom: 18,
 });
 
+const svg = d3.select('#map').select('svg');
+
 const bikeLaneStyle = {
   'line-color': '#32D400',
   'line-width': 5,
   'line-opacity': 0.6,
 };
 
-map.on('load', () => {
+function getCoords(station) {
+  const lon = station.lon ?? station.Long ?? station.long;
+  const lat = station.lat ?? station.Lat ?? station.latitude;
+  const point = new mapboxgl.LngLat(+lon, +lat);
+  const { x, y } = map.project(point);
+  return { cx: x, cy: y };
+}
+
+map.on('load', async () => {
   map.addSource('boston_route', {
     type: 'geojson',
     data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
@@ -43,4 +52,34 @@ map.on('load', () => {
     source: 'cambridge_route',
     paint: bikeLaneStyle,
   });
+
+  const jsonData = await d3.json('https://dsc106.com/labs/lab07/data/bluebikes-stations.json');
+  const stations = jsonData.data.stations;
+
+  console.log('Loaded JSON Data:', jsonData);
+  console.log('Stations Array:', stations);
+
+  const circles = svg
+    .selectAll('circle')
+    .data(stations)
+    .enter()
+    .append('circle')
+    .attr('r', 5)
+    .attr('fill', 'steelblue')
+    .attr('stroke', 'white')
+    .attr('stroke-width', 1)
+    .attr('opacity', 0.8);
+
+  function updatePositions() {
+    circles
+      .attr('cx', (d) => getCoords(d).cx)
+      .attr('cy', (d) => getCoords(d).cy);
+  }
+
+  updatePositions();
+
+  map.on('move', updatePositions);
+  map.on('zoom', updatePositions);
+  map.on('resize', updatePositions);
+  map.on('moveend', updatePositions);
 });
